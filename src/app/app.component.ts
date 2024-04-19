99042f2822f3fd3e34be3ccd0da96c32fee41a2a
dalyl <adidadi.adiden@gmail.com>
2024-04-19 08:24:47 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
+import {translate} from "@jsverse/transloco";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'TranslocoBugReport';
+  constructor() {
+    translate("This first key will be extracted");
+  }
+
+  getString(): string
+  {
+    return "5";
+  }
+
+  extractionProblem(): void
+  {
+    translate("This second key will be extracted");
+    const foo = <string>this.getString();
+    translate("This third key will NOT be extracted");
+  }
 }
